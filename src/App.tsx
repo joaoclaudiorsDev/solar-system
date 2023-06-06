a876752964d671ddebb5857e8916b7f6bca0645d
João Cláudio <joaoclaudiobio@gmail.com>
2023-06-06 09:12:11 -0300

    requisito 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 function App() {
   return (
---
+++ src/components/PlanetCard.tsx
@@ -0,0 +1,15 @@
+type PlanetProps = {
+    planetName: string;
+    planetImage: string;
+  }
+
+function PlanetCard({ planetName, planetImage }: PlanetProps) {
+    return (
+      <div data-testid="planet-card">
+        <img src= {planetImage} alt={`Planeta ${planetName}`}> </img>
+        <p data-testid="planet-name"> {planetName}</p>
+      </div>
+    );
+  }
+  
+  export default PlanetCard;
